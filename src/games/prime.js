ceffe02aa28307f8b93fbe90739099ebbb96df66
Leonid <nzleonid@gmail.com>
2018-12-30 18:33:33 +0700

    fix prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,11 +4,11 @@ import randomNum from '../utils';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (question) => {
-  const maxDivider = question / 2;
   if (question <= 1) {
     return false;
   }
-  for (let i = 2; i < maxDivider; i += 1) {
+  const maxDivider = question / 2;
+  for (let i = 2; i <= maxDivider; i += 1) {
     if (question % i === 0) {
       return false;
     }
