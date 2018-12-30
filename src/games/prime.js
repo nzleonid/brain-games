4e302d8d10a380ae5ab03d4fc9d63f5ece56f31d
Leonid <nzleonid@gmail.com>
2018-12-30 16:23:58 +0700

    refactoring prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,7 +5,7 @@ const description = 'Answer "yes" if given number is prime. Otherwise answer "no
 
 const isPrime = (question) => {
   const maxDivider = question / 2;
-  if (question === 1) {
+  if (question <= 1) {
     return false;
   }
   for (let i = 2; i < maxDivider; i += 1) {
